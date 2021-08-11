58a01e8ea6e6b998d566120ffeaaa2dabeab67e5
GuyInGrey <guyingreydaboss@gmail.com>
2021-08-11 14:48:57 -0600

    Here

---
+++ NFTGenerator/Generator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NFTGenerator
+{
+    public static class Generator
+    {
+        public static byte[] Generate(int token)
+        {
+
+        }
+    }
+}
---
+++ NFTInterface/Controllers/NFT.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+using NFTInterface.Models;
+
+namespace NFTInterface.Controllers
+{
+    public class NFT : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public NFT(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(int tokenID)
+        {
+            return File(new byte[] { (byte)tokenID }, "text/text");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
